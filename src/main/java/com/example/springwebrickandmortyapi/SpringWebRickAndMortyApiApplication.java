9b32f679e14198a31f8ab08433ac9deb69954bff
Ildrassil <schneiderjakob7@googlemail.com>
2024-01-08 15:36:35 +0100

    First step 080124 Challenge Nachmittag

---
+++ src/main/java/com/example/springwebrickandmortyapi/SpringWebRickAndMortyApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.springwebrickandmortyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringWebRickAndMortyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringWebRickAndMortyApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springwebrickandmortyapi/SpringWebRickAndMortyApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springwebrickandmortyapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringWebRickAndMortyApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
